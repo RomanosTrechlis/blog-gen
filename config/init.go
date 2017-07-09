35e95f1bd931c741e9ead42e8834bf12a5086515
Romanos Trechlis <r.trechlis@gmail.com>
2017-07-09 14:08:09 +0300

    config struct

--- config/init.go
+++ config/init.go
@@ -17,36 +17,42 @@ type BlogInformation interface {
 
 // SiteInformation contains the information inside ConfigFile
 type SiteInformation struct {
-	Author          string `json:Author`
-	BlogURL         string `json:BlogURL`
-	BlogLanguage    string `json:BlogLanguage`
-	BlogDescription string `json:BlogDescription`
-	DateFormat      string `json:DateFormat`
-	Theme           struct {
-		Repository string `json:Repository`
-		Type       string `json:Type`
-	}
+	Author            string `json:Author`
+	BlogURL           string `json:BlogURL`
+	BlogLanguage      string `json:BlogLanguage`
+	BlogDescription   string `json:BlogDescription`
+	DateFormat        string `json:DateFormat`
+	Theme             Theme
 	ThemeFolder       string `json:ThemeFolder`
 	BlogTitle         string `json:BlogTitle`
 	NumPostsFrontPage int    `json:NumPostsFrontPage`
-	DataSource        struct {
-		Type       string `json:Type`
-		Repository string `json:Repository`
-	}
-	Upload struct {
-		URL      string `json:URL`
-		Username string `json:Username`
-		Password string `json:Password`
-	}
-	TempFolder  string       `json:TempFolder`
-	DestFolder  string       `json:DestFolder`
-	StaticPages []StaticPage `json:StaticPages`
+	DataSource        DataSource
+	Upload            Upload
+	TempFolder        string       `json:TempFolder`
+	DestFolder        string       `json:DestFolder`
+	StaticPages       []StaticPage `json:StaticPages`
+}
+
+type Theme struct {
+	Repository string `json:Repository`
+	Type       string `json:Type`
 }
 
 type StaticPage struct {
 	File       string `json:File`
 	To         string `json:To`
 	IsTemplate bool   `json:IsTemplate`
+}
+
+type DataSource struct {
+	Type       string `json:Type`
+	Repository string `json:Repository`
+}
+
+type Upload struct {
+	URL      string `json:URL`
+	Username string `json:Username`
+	Password string `json:Password`
 }
 
 func NewSiteInformation(configFile string) (siteInfo SiteInformation) {
