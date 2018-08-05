0eb8bbdc76faaaf295e4a60034c38a3a0538dbb8
Romanos Trechlis <r.trechlis@gmail.com>
2018-08-05 17:35:49 +0300

    tests for util/fs package

--- util/fs/filesystem_test.go
+++ util/fs/filesystem_test.go
@@ -17,7 +17,7 @@ func TestCreateFolderIfNotExist(t *testing.T) {
 		removeFolder bool
 	}{
 		{filepath.Join("testdata", "existing"), "folder exists", false, false},
-		{filepath.Join("testdata", "notExisting"), "folder exists", false, true},
+		{filepath.Join("testdata", "notExisting"), "folder doesn't exist", false, true},
 	}
 
 	for _, tt := range tests {
@@ -75,6 +75,8 @@ func TestGetFilenameFrom(t *testing.T) {
 		{"folder\\folder\\file.txt", "file.txt"},
 		{"file.txt", "file.txt"},
 		{"\\file.txt", "file.txt"},
+		{"", ""},
+		{"folder\\file", ""},
 	}
 	if runtime.GOOS != "windows" {
 		tests = []struct {
@@ -86,6 +88,8 @@ func TestGetFilenameFrom(t *testing.T) {
 			{"folder/folder/file.txt", "file.txt"},
 			{"file.txt", "file.txt"},
 			{"/file.txt", "file.txt"},
+			{"", ""},
+			{"folder/file", ""},
 		}
 	}
 
@@ -107,6 +111,7 @@ func TestGetFolderNameFrom(t *testing.T) {
 		{"folder\\folder\\file.txt", "folder\\folder"},
 		{"file.txt", ""},
 		{"\\file.txt", ""},
+		{"", ""},
 	}
 	if runtime.GOOS != "windows" {
 		tests = []struct {
@@ -118,6 +123,7 @@ func TestGetFolderNameFrom(t *testing.T) {
 			{"folder/folder/file.txt", "folder/folder"},
 			{"file.txt", ""},
 			{"/file.txt", ""},
+			{"", ""},
 		}
 	}
 
@@ -125,6 +131,91 @@ func TestGetFolderNameFrom(t *testing.T) {
 		r := fs.GetFolderNameFrom(tt.path)
 		if r != tt.result {
 			t.Errorf("expected '%s', got '%s' (%s)", tt.result, r, tt.path)
+		}
+	}
+}
+
+func TestGetContentFolders(t *testing.T) {
+	var tests = []struct {
+		folder string
+		desc   string
+		err    bool
+		num    int
+	}{
+		{filepath.Join("testdata", "existing"), "folder exists and has 1 file", false, 0},
+		{filepath.Join("testdata", "notExisting"), "folder doesn't exist", true, 0},
+		{filepath.Join("testdata"), "folder exists and has 3 folders", false, 3},
+	}
+
+	for _, tt := range tests {
+		c, err := fs.GetContentFolders(tt.folder)
+		if err != nil && !tt.err {
+			t.Errorf("expected no error, got '%v'", err)
+		}
+		if err == nil && tt.err {
+			t.Error("expected error, got no error")
+		}
+		if err != nil {
+			continue
+		}
+
+		if len(c) != tt.num {
+			t.Errorf("expected %d number of files, got %d", tt.num, len(c))
+		}
+	}
+}
+
+func TestClearFolder(t *testing.T) {
+	var tests = []struct {
+		folder       string
+		err          bool
+		createFolder bool
+		removeFolder bool
+	}{
+		{filepath.Join("testdata", "tempFolder"), false, true,true},
+		{filepath.Join("testdata", "tempFolder"), true, false,false},
+	}
+
+	cp := filepath.Join("testdata", "existing")
+	for _, tt := range tests {
+
+		if tt.createFolder {
+			err := fs.CopyDir(cp, tt.folder)
+			if err != nil {
+				t.Errorf("failed to initialize test: copy of dir '%s' to dir '%s' failed: stopping test", cp, tt.folder)
+				continue
+			}
+		}
+
+		err := fs.ClearFolder(tt.folder)
+		if err != nil && !tt.err {
+			t.Errorf("expected no error, got '%v'", err)
+		}
+		if err == nil && tt.err {
+			t.Error("expected error, got no error")
+		}
+		if err != nil {
+			continue
+		}
+
+		dir, err := os.Open(tt.folder)
+		if err != nil {
+			t.Errorf("error accessing directory %s: %v", tt.folder, err)
+			continue
+		}
+		defer dir.Close()
+		files, err := dir.Readdir(-1)
+		if err != nil {
+			t.Errorf("error reading contents of directory %s: %v", tt.folder, err)
+			continue
+		}
+
+		if len(files) != 0 {
+			t.Errorf("expecting 0 files in directory %s, got %d", tt.folder, len(files))
+		}
+
+		if tt.removeFolder {
+			os.RemoveAll(tt.folder)
 		}
 	}
 }
