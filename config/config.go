4b965ef468cded4f26b300dcae7e4141b5692494
Romanos Trechlis <r.trechlis@gmail.com>
2018-08-05 12:24:40 +0300

    go-vet on config package

--- config/config.go
+++ config/config.go
@@ -13,43 +13,43 @@ type BlogInformation interface {
 
 // SiteInformation contains the information inside ConfigFile
 type SiteInformation struct {
-	Author            string `json:Author`
-	BlogURL           string `json:BlogURL`
-	BlogLanguage      string `json:BlogLanguage`
-	BlogDescription   string `json:BlogDescription`
-	DateFormat        string `json:DateFormat`
+	Author            string `json:"Author"`
+	BlogURL           string `json:"BlogURL"`
+	BlogLanguage      string `json:"BlogLanguage"`
+	BlogDescription   string `json:"BlogDescription"`
+	DateFormat        string `json:"DateFormat"`
 	Theme             Theme
-	ThemeFolder       string `json:ThemeFolder`
-	BlogTitle         string `json:BlogTitle`
-	NumPostsFrontPage int    `json:NumPostsFrontPage`
+	ThemeFolder       string `json:"ThemeFolder"`
+	BlogTitle         string `json:"BlogTitle"`
+	NumPostsFrontPage int    `json:"NumPostsFrontPage"`
 	DataSource        DataSource
 	Upload            Upload
-	TempFolder        string       `json:TempFolder`
-	DestFolder        string       `json:DestFolder`
-	StaticPages       []StaticPage `json:StaticPages`
+	TempFolder        string       `json:"TempFolder"`
+	DestFolder        string       `json:"DestFolder"`
+	StaticPages       []StaticPage `json:"StaticPages"`
 }
 
 type Theme struct {
-	Repository string `json:Repository`
-	Type       string `json:Type`
+	Repository string `json:"Repository"`
+	Type       string `json:"Type"`
 }
 
 type StaticPage struct {
-	File       string `json:File`
-	To         string `json:To`
-	IsTemplate bool   `json:IsTemplate`
+	File       string `json:"File"`
+	To         string `json:"To"`
+	IsTemplate bool   `json:"IsTemplate"`
 }
 
 type DataSource struct {
-	Type       string `json:Type`
-	Repository string `json:Repository`
+	Type       string `json:"Type"`
+	Repository string `json:"Repository"`
 }
 
 type Upload struct {
-	Type     string `json:Type`
-	URL      string `json:URL`
-	Username string `json:Username`
-	Password string `json:Password`
+	Type     string `json:"Type"`
+	URL      string `json:"URL"`
+	Username string `json:"Username"`
+	Password string `json:"Password"`
 }
 
 func New(configFile string) (SiteInformation, error) {
@@ -58,12 +58,12 @@ func New(configFile string) (SiteInformation, error) {
 		return SiteInformation{}, fmt.Errorf("error accessing directory %s: %v", configFile, err)
 	}
 	siteInfo := new(SiteInformation)
-	siteInfo.ParseJSON(data)
+	siteInfo.parseJSON(data)
 	siteInfo.fillDefaultValues()
 	return *siteInfo, nil
 }
 
-func (si *SiteInformation) ParseJSON(b []byte) (err error) {
+func (si *SiteInformation) parseJSON(b []byte) (err error) {
 	return json.Unmarshal(b, &si)
 }
 
